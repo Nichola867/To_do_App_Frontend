07b72188e83389e8afadc1a08fbf06f4be16f998
Nichola Evans <nichola867@hotmail.com>
2019-12-10 20:09:55 +0000

    States added to To Do App

--- src/App.js
+++ src/App.js
@@ -5,47 +5,79 @@ import Task from "./Task";
 import CompletedTasks from "./CompletedTasks";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <WriteTask />
+class App extends React.Component {
+  state = {
+    tasks: [
+      { dateAdded: "13/11/19", task: "Feed the cat", completed: false, ID: 1 },
+      { dateAdded: "19/11/19", task: "Do laundry", completed: true, ID: 2 },
+      { dateAdded: "19/11/19", task: "File notes from lecture", completed: false, ID: 3 },
+      { dateAdded: "19/11/19", task: "Put clothes away", completed: false, ID: 4 },
+      { dateAdded: "19/11/19", task: "Buy cat food", completed: true, ID: 5 },
+      { dateAdded: "19/11/19", task: "Write up notes from lecture", completed: true, ID: 6 },
+    ]
+  }
 
-      <div className="container">
+  render() {
+    const completedTasks = this.state.tasks.filter(n => {
+      return n.completed === true;
+    });
 
-        <div className="row">
-          <div className="col-12 col-lg-6">
-            <ul className="list-group">
-              <li className="list-group-item d-flex justify-content-between align-items-center TaskHeaders">
-                <i className="fas fa-list-ul" /> <span>Tasks to do </span>
-                <span className="badge badge-light badge-pill">3</span>
-              </li>
-            </ul>
+    const toDoTasks = this.state.tasks.filter(n => {
+      return n.completed === false;
+    });
 
-            <Task dateAdded="03/12/19" task="Feed the cat" />
-            <Task dateAdded="01/12/19" task="File notes from lecture" />
-            <Task dateAdded="18/11/19" task="Put clothes away" />
-          </div>
+    return (
+      <div className="App" >
+        <Header />
+        <WriteTask />
 
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-6">
 
+              <ul className="list-group">
+                <li className="list-group-item d-flex justify-content-between align-items-center TaskHeaders">
+                  <i className="fas fa-list-ul" /> <span> Tasks to do </span>
+                  <span className="badge badge-light badge-pill"> 3 </span>
+                </li>
+              </ul>
 
-          <div className="col-12 col-lg-6">
-            <ul className="list-group">
-              <li className="list-group-item d-flex justify-content-between align-items-center TaskHeaders">
-                <i className="fas fa-tasks" /> <span> Completed tasks </span>
+              {toDoTasks.map(n => {
+                return (
+                  <Task
+                    key={n.ID}
+                    dateAdded={n.dateAdded}
+                    task={n.task}
+                  />
+                )
+              })}
+            </div>
+
+            <div className="col-12 col-lg-6">
+              <ul className="list-group">
+                <li className="list-group-item d-flex justify-content-between align-items-center TaskHeaders">
+                  <i className="fas fa-tasks" /> <span> Completed tasks </span>
                   <span className="badge badge-light badge-pill">3</span>
-              </li>
-            </ul>
+                </li>
+              </ul>
 
-              <CompletedTasks dateCompleted="03/11/19" taskCompleted="Buy cat food" />
-              <CompletedTasks dateCompleted="01/10/19" taskCompleted="Write up notes from lecture" />
-              <CompletedTasks dateCompleted="14/11/19" taskCompleted="Do laundry" />
+              {completedTasks.map(n => {
+                return (
+                  <Task
+                    Key={n.ID}
+                    dateAdded={n.dateAdded}
+                    task={n.task}
+                  />
+                )
+              })}
+            </div>
           </div>
 
-          </div>
         </div>
+
       </div>
-      );
-    }
-    
-    export default App;
+    )
+  }
+}
+
+export default App;
