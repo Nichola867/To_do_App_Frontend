a0152abc076e8e6b3a2451670eba944f8e34db15
Nichola Evans <nichola867@hotmail.com>
2019-12-10 21:41:28 +0000

    event.target.value added to the text box

--- src/WriteTask.js
+++ src/WriteTask.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 class WriteTask extends React.Component {
+
+    state = {
+        dateAdded: "10/12/19",
+        task: "",
+    };
+
+    updateTask = event => {
+        console.log(event.target.value)
+
+         this.setState({
+        task: event.target.value
+       });
+    };
+
     render() {
         return (
             <div className="container">
@@ -8,10 +22,20 @@ class WriteTask extends React.Component {
                     <div className="col-9 ">
 
                         <div className="form-group myTaskFont">
-                            <label form="exampleFormControlTextarea1">My next task...</label>
-                            <textarea className="form-control myNextTask" id="exampleFormControlTextarea1 " rows="2"> </textarea>
-                        </div>
+                            <label form="exampleFormControlTextarea1" >
+                                My next task...
+                                </label>
+                       
+
+                        <textarea className="form-control myNextTask"
+                            onChange={this.updateTask}
+                            value={this.state.task}
+                            id="exampleFormControlTextarea1"
+                            rows="2" >
+                        </textarea>
+
                     </div>
+                     </div>
 
                     <div className="col-3">
                         <button className="btn btn-primary addTask-button" type="submit">Add task</button>
@@ -22,4 +46,8 @@ class WriteTask extends React.Component {
     }
 }
 
+
 export default WriteTask;
+
+
+
