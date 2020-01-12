f766e83c8f3e2dfbf3fb10e4a27e1a2df98572e9
Nichola Evans <nichola867@hotmail.com>
2020-01-12 15:19:44 +0000

    Updates

---
+++ precache-manifest.a9858e6a7a18faea8e9cff171fe19250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e099583fc0ce1db7eabf2f49084a6bd8",
+    "url": "/To_do_App_Frontend/index.html"
+  },
+  {
+    "revision": "2d3d56677d31b54837a3",
+    "url": "/To_do_App_Frontend/static/css/main.9d241135.chunk.css"
+  },
+  {
+    "revision": "d65a041ac8c282320ebf",
+    "url": "/To_do_App_Frontend/static/js/2.89f2739e.chunk.js"
+  },
+  {
+    "revision": "2d3d56677d31b54837a3",
+    "url": "/To_do_App_Frontend/static/js/main.78606be6.chunk.js"
+  },
+  {
+    "revision": "2fe16a5dbfdc15d580cf",
+    "url": "/To_do_App_Frontend/static/js/runtime-main.1d88fdb0.js"
+  }
+]);
